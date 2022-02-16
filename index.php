523afa90c3a26ec3398fe50c2e5bb2f0f982ebf9
Will H <77733928+ProtonNumber@users.noreply.github.com>
2022-02-16 12:38:16 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Maths Revison Tool</title>
+<link rel="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="CentreDiv">
+	<p class="CentreText">Are you a teacher or a student?</p>
+	<div class="IndexButtonContainer">
+		<button class="IndexButton" onclick="location.href='student/login.php'">Student</button> <button class="IndexButton">Teacher</button>
+	</div>
+</div>
+</body>
+</html>
